e0d3b50dccfd58aa3ef534a77d40f3c2caace31c
Alexandr Rudenko <alexandr.rudenko@protonmail.com>
2020-10-10 15:03:35 +0200

    Updates

---
+++ precache-manifest.7bbe9584e5c555a9cc2143fd88fbaf2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d2cc5d1939de0dc123d8a104e94f40",
+    "url": "/AlexRedwood.github.io/cv-project/index.html"
+  },
+  {
+    "revision": "cc0f4cd3f15e991cdba5",
+    "url": "/AlexRedwood.github.io/cv-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "11b00e0e5885fb88b968",
+    "url": "/AlexRedwood.github.io/cv-project/static/js/2.93fbed03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlexRedwood.github.io/cv-project/static/js/2.93fbed03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0f4cd3f15e991cdba5",
+    "url": "/AlexRedwood.github.io/cv-project/static/js/main.434f050b.chunk.js"
+  },
+  {
+    "revision": "f5e253ed08fa726d2c09",
+    "url": "/AlexRedwood.github.io/cv-project/static/js/runtime-main.59ea07b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlexRedwood.github.io/cv-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
